d976f2142757fbf080315bf177c21d049d71e94c
Andrew Calimlim <andr3wcalimlim@gmail.com>
2023-05-22 22:23:28 -0700

    Upload Button on Scan Menu has a warning alert now

--- app/src/main/java/com/example/kanjinokanji/MainMenu.java
+++ app/src/main/java/com/example/kanjinokanji/MainMenu.java
@@ -2,32 +2,41 @@ package com.example.kanjinokanji;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity { //LMAO IT WAS JUST THE NAME
+
+    protected void displayScanButton(Context c){
+        Button scanMenuButton = (Button) findViewById(R.id.main_scanButton);
+        Log.d("VALID", Integer.toString(R.id.main_scanButton));
+        // this unsightly mess is how u change activities in Java btw
+        // https://developer.android.com/reference/android/content/Context#startActivity(android.content.Intent)
+
+        scanMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(c, ScanMenu.class));
+            }
+        });
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //ImageProcessing.imageProcess(this, "romantic");
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_main_menu); // note this has to be called
+        displayScanButton(MainMenu.this); //before grabbing views by ID like here
+        // cause the view object hasn't been made yet otherwise
 
-        Button scanMenuButton = (Button) findViewById(R.id.main_scanButton);
-
-        // this unsightly mess is how u change activities in Java btw
-        // https://developer.android.com/reference/android/content/Context#startActivity(android.content.Intent)
-
-
-        scanMenuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainMenu.this, ScanMenu.class));
-            }
-        });
 
 
 
--- app/src/main/java/com/example/kanjinokanji/ScanMenu.java
+++ app/src/main/java/com/example/kanjinokanji/ScanMenu.java
@@ -1,7 +1,12 @@
 package com.example.kanjinokanji;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ScanMenu extends AppCompatActivity{
 
@@ -10,5 +15,42 @@ public class ScanMenu extends AppCompatActivity{
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_menu);
+
+        Button uploadMenuButton = (Button) findViewById(R.id.scan_uploadButton);
+        uploadMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String uploadReminderMessage = "When uploading an image, please crop it as much" +
+                        " as you can.";
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(ScanMenu.this);
+                builder.setTitle("Upload Reminder");
+                builder.setMessage(uploadReminderMessage);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                builder.setNegativeButton("NVM then", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        });
+
+
+
+
+
     }
 }
