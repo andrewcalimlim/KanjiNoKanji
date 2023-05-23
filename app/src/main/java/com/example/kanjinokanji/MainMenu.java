97490981fd3c563ca0c239db7b408acbaa468f8d
Andrew Calimlim <andr3wcalimlim@gmail.com>
2023-05-22 19:41:59 -0700

    Scan Button on Main Menu has functionality

--- app/src/main/java/com/example/kanjinokanji/MainMenu.java
+++ app/src/main/java/com/example/kanjinokanji/MainMenu.java
@@ -1,7 +1,11 @@
 package com.example.kanjinokanji;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity { //LMAO IT WAS JUST THE NAME
 
@@ -11,5 +15,22 @@ public class MainMenu extends AppCompatActivity { //LMAO IT WAS JUST THE NAME
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button scanMenuButton = (Button) findViewById(R.id.main_scanButton);
+
+        // this unsightly mess is how u change activities in Java btw
+        // https://developer.android.com/reference/android/content/Context#startActivity(android.content.Intent)
+
+
+        scanMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenu.this, ScanMenu.class));
+            }
+        });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/kanjinokanji/ScanMenu.java
+++ app/src/main/java/com/example/kanjinokanji/ScanMenu.java
@@ -9,6 +9,6 @@ public class ScanMenu extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main_menu);
+        setContentView(R.layout.activity_scan_menu);
     }
 }
