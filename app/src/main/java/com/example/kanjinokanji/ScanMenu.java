07820876f6ac284a2015a8ef215150024819e691
Andrew Calimlim <andr3wcalimlim@gmail.com>
2023-07-13 23:28:35 -0700

    updated dependencies to import ActivityResultContracts

--- app/src/main/java/com/example/kanjinokanji/ScanMenu.java
+++ app/src/main/java/com/example/kanjinokanji/ScanMenu.java
@@ -1,14 +1,17 @@
 package com.example.kanjinokanji;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class ScanMenu extends AppCompatActivity{
 
@@ -17,6 +20,8 @@ public class ScanMenu extends AppCompatActivity{
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_menu);
+
+        int SELECT_PICTURE = 200;
 
         Button uploadMenuButton = (Button) findViewById(R.id.scan_uploadButton);
         uploadMenuButton.setOnClickListener(new View.OnClickListener() {
@@ -42,8 +47,29 @@ public class ScanMenu extends AppCompatActivity{
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
+                        //ActivityResultLauncher<Pick> pickMedia;
+                        //https://developer.android.com/training/data-storage/shared/photopicker
 
-                    }
+                        // use this tutorial
+
+                        // PhotoPicker activity will be in single-select mode
+                        ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
+                                registerForActivityResult(new PickVisualMedia(), uri -> {
+                                    if (uri != null){
+                                        Log.d("PhotoPicker", "Selected URI: " + uri);
+                                    } else {
+                                        Log.d("PhotoPicker", "No media selected");
+                                    }
+                        });
+                        /*
+                        pickMedia.launch(new PickVisualMediaRequest.Builder()
+                                .setMediaType(PickVisualMedia.ImageOnly.INSTANCE)
+                                .build());
+                        */
+
+
+
+                }
                 });
 
                 AlertDialog dialog = builder.create();
